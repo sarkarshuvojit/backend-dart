c7191438f73d1ed748042b96ce3044167e56ffa4
Shuvojit <s15sarkar@yahoo.com>
2024-01-02 21:15:45 +0530

    implemented basic constructor

--- lib/src/backend_dart_base.dart
+++ lib/src/backend_dart_base.dart
@@ -1,6 +1,12 @@
 // TODO: Put public facing types in this file.
 
 /// Checks if you are awesome. Spoiler: you are.
-class Awesome {
-  bool get isAwesome => true;
+class Backend {
+  String _publicKey;
+  String _region;
+
+  Backend(this._publicKey, this._region);
+
+  String get publicKey => _publicKey;
+  String get region => _region;
 }
--- test/backend_dart_test.dart
+++ test/backend_dart_test.dart
@@ -2,15 +2,15 @@ import 'package:backend_dart/backend_dart.dart';
 import 'package:test/test.dart';
 
 void main() {
-  group('A group of tests', () {
-    final awesome = Awesome();
+  group('initialisation tests', () {
+    final backend = Backend("your-pub-key", "local");
 
     setUp(() {
       // Additional setup goes here.
     });
 
-    test('First Test', () {
-      expect(awesome.isAwesome, isTrue);
+    test('Tests whether region matches the input provided', () {
+      expect(backend.region, "local");
     });
   });
 }
