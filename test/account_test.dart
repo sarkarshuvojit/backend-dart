feb637e0bdfe1af561edb13a3c8e8e4de51bf1c3
Shuvojit <s15sarkar@yahoo.com>
2024-01-11 07:55:54 +0530

    added test for login & cleaned up a bit

--- test/account_test.dart
+++ test/account_test.dart
@@ -12,7 +12,7 @@ import 'account_test.mocks.dart' as test_mocks;
 
 final mockPublicKey = "fakePpublicKey";
 
-Backend getMockBackendFromMockClient(test_mocks.MockClient mockClient) {
+Backend getBackendWithMockClient(test_mocks.MockClient mockClient) {
   return Backend.forTest(
         mockPublicKey,
         "dev", 
@@ -24,12 +24,12 @@ Backend getMockBackendFromMockClient(test_mocks.MockClient mockClient) {
 void main() {
   group('Testing Accounts APIs', () {
 
-    test('Regiser should call /register with correct credentials', () async {
+    test('Regiser should call /register with correct credentials; and token should be returned', () async {
       final mockClient = test_mocks.MockClient();
       final mockJwt = "eyfirstpart.second.third";
       final mockEmail = "shuvojit@test.com";
       final mockPassword = "shuvojit";
-      Backend backend = getMockBackendFromMockClient(mockClient);      
+      Backend backend = getBackendWithMockClient(mockClient);      
 
       when(
         mockClient.post(
@@ -45,7 +45,31 @@ void main() {
         )
       ).thenAnswer((_) async => http.Response(mockJwt, 200));
 
-      expect(await backend.register(mockEmail, mockPassword), mockJwt);
+      expect(await backend.account.register(mockEmail, mockPassword), mockJwt);
+    });
+
+    test('Login should call /login with correct credentials, and token should be returned', () async {
+      final mockClient = test_mocks.MockClient();
+      final mockJwt = "eyfirstpart.second.third";
+      final mockEmail = "shuvojit@test.com";
+      final mockPassword = "shuvojit";
+      Backend backend = getBackendWithMockClient(mockClient);      
+
+      when(
+        mockClient.post(
+          Uri.parse("http://testbaseurl/login"), 
+          body: jsonEncode({
+            "email": mockEmail,
+            "password": mockPassword
+          }), 
+          headers: {
+            "SB-PUBLIC-KEY": mockPublicKey
+          }, 
+          encoding: null
+        )
+      ).thenAnswer((_) async => http.Response(mockJwt, 200));
+
+      expect(await backend.account.login(mockEmail, mockPassword), mockJwt);
     });
 
   });
